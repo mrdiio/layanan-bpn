690198cf6cbc63ffad80d0f30882127a329fdf68
J0KER <dionatsir@gmail.com>
2019-08-14 22:41:47 +0700

    Update navbar.blade.php

--- resources/views/layouts/parts/navbar.blade.php
+++ resources/views/layouts/parts/navbar.blade.php
@@ -21,8 +21,6 @@
                 </a>
 
                 <ul class="dropdown-menu dropdown-menu-right">
-                    <li><a href="#"><i class="icon-user"></i> My profile</a></li>
-                    <li class="divider"></li>
                     <li>
                         <a href="{{ route('logout') }}"
                             onclick="event.preventDefault();
