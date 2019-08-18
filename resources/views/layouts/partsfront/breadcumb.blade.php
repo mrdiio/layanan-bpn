8aec20382a63d3b3a92c82d0959147881f4d2cdd
rizqimauludin <rizqiedge.rm@gmail.com>
2019-08-18 20:08:44 +0700

    breadcrumb

--- resources/views/layouts/partsfront/breadcumb.blade.php
+++ resources/views/layouts/partsfront/breadcumb.blade.php
@@ -4,6 +4,7 @@
         <div class="c-page-title c-pull-left">
             <h3 class="c-font-uppercase c-font-bold c-font-white c-font-20 c-font-slim">BADAN PERTANAHAN NASIONAL</h3>
             <h4 class="c-font-white c-font-thin c-opacity-07">KABUPATEN KUBU RAYA</h4>
+            <h4 class="c-font-white c-font-thin c-opacity-07">Jl. Adi Sucipto No. 79, Arang Limbung, Kec. Sungai Raya, Kab. Kubu Raya, Kalimantan Barat (78391)</h4>
         </div>
     </div>
 </div><!-- END: LAYOUT/BREADCRUMBS/BREADCRUMBS-3 -->
