6ed229dff5a4de7e0a949bad546e33ebfd3e5492
J0KER <dionatsir@gmail.com>
2019-08-18 17:47:03 +0700

    tambah alamat
    alamat tambah di dalam gambar

--- resources/views/layouts/partsfront/breadcumb.blade.php
+++ resources/views/layouts/partsfront/breadcumb.blade.php
@@ -1,4 +1,4 @@
-<div class="c-layout-breadcrumbs-1 c-bgimage-full  c-centered  c-fonts-uppercase c-fonts-bold   c-bg-img-center" style="background-image: url(images/bpn.jpg)">
+<div class="c-layout-breadcrumbs-1 c-bgimage-full  c-centered  c-fonts-uppercase c-fonts-bold   c-bg-img-center" style="background-image: url(images/bpn-address.jpg)">
     <div class="container" style="height:90px">
     </div>
 </div>
